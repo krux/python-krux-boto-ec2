d8ca17a5438064f954cdd83aa081c17cbb7f2508
Peter Han <phan@krux.com>
2016-10-13 18:19:10 -0700

    Updating filter so that it can handle keyword arguments as well

--- krux_ec2/filter.py
+++ krux_ec2/filter.py
@@ -24,39 +24,44 @@ class Filter(MutableMapping):
     .. seealso:: https://boto3.readthedocs.io/en/stable/reference/services/ec2.html#EC2.ServiceResource.instances
 
     :example:
-    You can use this in 3 different ways:
-        1. Pass a dictionary of filter criteria and values to the constructor
-        2. Create an empty filter and add criteria using add_filter or add_tag_filter methods
-        3. Create an empty filter and add creteria from string using parse_string method
+        You can use this in 3 different ways:
+            1. Pass a dictionary of filter criteria and values to the constructor
+            2. Create an empty filter and add criteria using add_filter or add_tag_filter methods
+            3. Create an empty filter and add creteria from string using parse_string method
 
-    >>> f1 = Filter({'tag:Name': ['example.krxd.net'], 'instance-state-name': ['running', 'stopped']})
+        >>> f1 = Filter({'tag:Name': ['example.krxd.net'], 'instance-state-name': ['running', 'stopped']})
 
-    >>> f2 = Filter()
-    >>> f2.add_tag_filter('Name', 'example.krxd.net')
-    >>> f2.add_filter('instance-state-name', 'running')
-    >>> f2.add_filter('instance-state-name', 'stopped')
+        >>> f2 = Filter()
+        >>> f2.add_tag_filter('Name', 'example.krxd.net')
+        >>> f2.add_filter('instance-state-name', 'running')
+        >>> f2.add_filter('instance-state-name', 'stopped')
 
-    >>> f3 = Filter()
-    >>> f3.parse_string('tag:Name=example.krxd.net')
-    >>> f3.parse_string('instance-state-name=running')
-    >>> f3.parse_string('instance-state-name=stopped')
+        >>> f3 = Filter()
+        >>> f3.parse_string('tag:Name=example.krxd.net')
+        >>> f3.parse_string('instance-state-name=running')
+        >>> f3.parse_string('instance-state-name=stopped')
 
-    All 3 above filters return a list of items that have 'example.krxd.net' value for the 'Name' tag
-    AND is either 'running' OR 'stopped'.
+        All 3 above filters return a list of items that have 'example.krxd.net' value for the 'Name' tag
+        AND is either 'running' OR 'stopped'.
+
+        Furthermore, for a quick and dirty solution, you can use keyword argument of the __init__().
+
+        >>> f4 = Filter(foo='bar')
     """
 
-    def __init__(self, initial=None):
+    def __init__(self, *args, **kwargs):
         """
         The initial parameter must be a dictionary of string to a list of values.
         The key is the filter criteria name, and the value is a list of matches.
 
-        :param initial: Initial set of filters to start with
-        :type initial: dict | None
+        :param args: Ordered arguments. If there is one, the first argument is used as the basis of this filter
+        :type args: list
+        :param kwargs: Keyword arguments. All keyword arguments are added to this filter
+        :type kwargs: dict
         """
-        self._filter = {}
+        self._filter = args[0] if len(args) > 0 else {}
 
-        if initial is not None:
-            self._filter.update(initial)
+        self._filter.update(kwargs)
 
     def __getitem__(self, key):
         return self._filter[key]
@@ -126,7 +131,10 @@ class Filter(MutableMapping):
 
     def to_filter(self):
         """
-        Override so that dict(Filter) returns the dict to be used by boto.
+        Returns a list of dictionaries that can be passed to Boto3's filtering methods.
+
+        :return: A list of dictionaries in a format expected by Boto3.
+        :rtype: list[dict]
         """
         return [
             {'Name': key, 'Values': values}
