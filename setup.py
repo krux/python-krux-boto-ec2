34a2933f37f5a4b8c2d44e9aa730dedcb709d49d
Rich Braun <rich.braun@salesforce.com>
2021-05-05 14:57:31 -0700

    W-8909569 adjust Pipfile (#22)
    * W-8909569 adjust Pipfile

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from __future__ import absolute_import
 from setuptools import setup, find_packages
 
 # We use the version to construct the DOWNLOAD_URL.
-VERSION = '0.5.3'
+VERSION = '0.5.4'
 
 # URL to the repository on Github.
 REPO_URL = 'https://github.com/krux/python-krux-boto-ec2'
